9df88c04c8c00865e438720ad5159094dabada0a
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2020-02-09 20:12:20 +0100

    Add files via upload

---
+++ buricmariopripremazaispit2zadatak/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace buricmariopripremazaispit2zadatak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPrikaz = new System.Windows.Forms.Button();
+            this.textBoxIme = new System.Windows.Forms.TextBox();
+            this.textBoxPrezime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonPrikaz
+            // 
+            this.buttonPrikaz.Location = new System.Drawing.Point(237, 101);
+            this.buttonPrikaz.Name = "buttonPrikaz";
+            this.buttonPrikaz.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrikaz.TabIndex = 0;
+            this.buttonPrikaz.Text = "Prikaz";
+            this.buttonPrikaz.UseVisualStyleBackColor = true;
+            this.buttonPrikaz.Click += new System.EventHandler(this.buttonPosalji_Click);
+            // 
+            // textBoxIme
+            // 
+            this.textBoxIme.Location = new System.Drawing.Point(274, 26);
+            this.textBoxIme.Name = "textBoxIme";
+            this.textBoxIme.Size = new System.Drawing.Size(100, 20);
+            this.textBoxIme.TabIndex = 1;
+            // 
+            // textBoxPrezime
+            // 
+            this.textBoxPrezime.Location = new System.Drawing.Point(274, 54);
+            this.textBoxPrezime.Name = "textBoxPrezime";
+            this.textBoxPrezime.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrezime.TabIndex = 2;
+            this.textBoxPrezime.TextChanged += new System.EventHandler(this.textBoxPrezime_TextChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ime:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(176, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Prezime:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPrezime);
+            this.Controls.Add(this.textBoxIme);
+            this.Controls.Add(this.buttonPrikaz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPrikaz;
+        private System.Windows.Forms.TextBox textBoxIme;
+        private System.Windows.Forms.TextBox textBoxPrezime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ buricmariopripremazaispit2zadatak/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buricmariopripremazaispit2zadatak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxIme_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxPrezime_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonPosalji_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBoxIme.Text + " " + textBoxPrezime.Text);
+        }
+
+        private void buttonPosalji_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxPrezime_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+        
+}
+
+    }
+
+
+
